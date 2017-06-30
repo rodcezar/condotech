62b2f7865ba72ff7202c67a4edcfc947076dcf32
rodcezar <rodrigo.cezar@gmail.com>
2017-06-30 13:13:25 -0300

    Update condotech.sql
    Updates and fixes

--- postgresql/condotech.sql
+++ postgresql/condotech.sql
@@ -88,15 +88,14 @@ BEGIN
 	  
 	IF PROPRIETARIO IS NULL OR PROPRIETARIO = '' 
 	THEN
-	    RETURN 'ADMINISTRADORA';
+	    RETURN 'À VENDA';
 	ELSIF COTA_CLIENTE < 100 THEN
-            RETURN PROPRIETARIO || ' (MAIOR COTISTA %' || COTA_CLIENTE::TEXT || ')';
+        RETURN PROPRIETARIO || ' (COTISTA PRINCIPAL %' || COTA_CLIENTE::TEXT || ')';
 	END IF;
 		
 	RETURN PROPRIETARIO;
 END;
 $$ LANGUAGE plpgsql;
-
 
 
 CREATE OR REPLACE FUNCTION insere_cota(pCpf TEXT, pId_predio int, pNumero int, pQuantidade int) RETURNS TEXT AS $$
@@ -172,7 +171,7 @@ BEGIN
 			UPDATE PREDIO
 			SET CPF_SINDICO = $2
 			WHERE ID_PREDIO = $1;
-			RETURN 'ATUALIZADO!';
+			RETURN 'SINDICO ATUALIZADO!';
 		ELSE
 			RETURN 'MESMO SÍNDICO';
 		END IF;
@@ -181,6 +180,117 @@ BEGIN
 	END IF;
 END;
 $$ LANGUAGE plpgsql;
+
+
+CREATE OR REPLACE FUNCTION sindico_audit() RETURNS TRIGGER AS $cota$
+DECLARE
+	CURRENT_SINDICO TEXT;
+BEGIN
+	IF (TG_OP = 'UPDATE') THEN
+		IF OLD.QUANTIDADE = 100 AND NEW.QUANTIDADE < 100 THEN
+
+			SELECT CPF INTO CURRENT_SINDICO
+			FROM COTA
+			WHERE QUANTIDADE = 100;
+
+			IF NOT FOUND THEN
+
+				UPDATE PREDIO
+				SET CPF_SINDICO = '000.000.000-00' 
+				WHERE ID_PREDIO = OLD.ID_PREDIO;
+				
+			END IF;
+
+		END IF;
+	END IF;
+			
+	IF (TG_OP = 'DELETE') THEN
+
+		SELECT CPF INTO CURRENT_SINDICO
+		FROM COTA
+		WHERE QUANTIDADE = 100;
+
+		IF NOT FOUND THEN
+
+			UPDATE PREDIO
+			SET CPF_SINDICO = '000.000.000-00'  
+			WHERE ID_PREDIO = OLD.ID_PREDIO;
+
+		END IF;
+
+	END IF;
+
+	RETURN NEW;
+
+END;
+$cota$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION check_cpf(p_cpf TEXT) RETURNS BOOLEAN AS $$
+DECLARE
+	WS_CPF TEXT;
+BEGIN
+	SELECT C.CPF INTO WS_CPF
+	FROM CLIENTE C 
+	WHERE C.CPF = $1;
+
+	IF FOUND THEN
+		RETURN TRUE;
+	ELSE
+		RETURN FALSE;
+	END IF;
+	
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION delete_predio(p_id_predio INT) RETURNS BOOLEAN AS $$
+DECLARE
+	AFFECTEDROWS INT;
+BEGIN
+	DELETE FROM COTA WHERE id_predio = $1;
+
+	DELETE FROM APARTAMENTO WHERE id_predio = $1; 
+	
+	WITH C AS (DELETE FROM PREDIO WHERE id_predio = $1 RETURNING 1)
+	SELECT COUNT(*) INTO AFFECTEDROWS FROM C;
+	IF AFFECTEDROWS = 1 THEN
+		RETURN TRUE;
+	ELSE
+		RETURN FALSE;
+	END IF;
+	
+END;
+$$ LANGUAGE plpgsql;
+
+
+CREATE OR REPLACE FUNCTION get_percentual_vendido(id_condominio int) RETURNS INT AS $$
+DECLARE
+	TOTAL_CONDOMINIO INT;
+	TOTAL_VENDIDO INT;	
+BEGIN
+
+	SELECT SUM(A.QUANTIDADE) INTO TOTAL_VENDIDO
+	FROM COTA A, PREDIO B 
+	WHERE A.ID_PREDIO = B.ID_PREDIO 
+	  AND B.ID_CONDOMINIO = $1;
+
+	SELECT COUNT(*)*100 TOTAL_VENDIDO INTO TOTAL_CONDOMINIO
+	FROM APARTAMENTO A, PREDIO B
+	WHERE A.ID_PREDIO = B.ID_PREDIO 
+	  AND B.ID_CONDOMINIO = $1;
+
+	RETURN (TOTAL_VENDIDO*100)/TOTAL_CONDOMINIO;
+	  
+  
+END;
+$$ LANGUAGE plpgsql;
+
+
+
+-- TRIGGERS -- 
+
+CREATE TRIGGER cota_update
+    AFTER UPDATE OR DELETE ON cota
+    FOR EACH ROW EXECUTE PROCEDURE sindico_audit();
 
 -- INSERT STUFF
 
@@ -194,7 +304,10 @@ insert into CLIENTE
 values('000.000.000-00', 'Administradora', 1, '(21)9999-9999', '<unknown>');
 
 insert into CLIENTE
-values('123.456.789-11', 'Luke Skywalker', 1, '(21)9876-1234', '22-04.1980');
+values('123.456.789-11', 'Luke Skywalker', 1, '(21)9876-1234', '01/01/1900');
+
+insert into CLIENTE
+values('222.222.222-22', 'Rodrigo', 1, '(21)7777-8888', '01/01/1900');
 
 insert into CONDOMINIO 
 values(1, 1, 'UNIRIO', '999.888.777-66');
@@ -233,6 +346,56 @@ insert into apartamento
 values(1001, 205);
 insert into apartamento
 values(1001, 206);
+
+insert into apartamento
+values(1002, 101);
+insert into apartamento
+values(1002, 102);
+insert into apartamento
+values(1002, 103);
+insert into apartamento
+values(1002, 104);
+insert into apartamento
+values(1002, 105);
+insert into apartamento
+values(1002, 106);
+insert into apartamento
+values(1002, 201);
+insert into apartamento
+values(1002, 202);
+insert into apartamento
+values(1002, 203);
+insert into apartamento
+values(1002, 204);
+insert into apartamento
+values(1002, 205);
+insert into apartamento
+values(1002, 206);
+
+insert into apartamento
+values(1003, 101);
+insert into apartamento
+values(1003, 102);
+insert into apartamento
+values(1003, 103);
+insert into apartamento
+values(1003, 104);
+insert into apartamento
+values(1003, 105);
+insert into apartamento
+values(1003, 106);
+insert into apartamento
+values(1003, 201);
+insert into apartamento
+values(1003, 202);
+insert into apartamento
+values(1003, 203);
+insert into apartamento
+values(1003, 204);
+insert into apartamento
+values(1003, 205);
+insert into apartamento
+values(1003, 206);
 
 insert into COTA
 values('999.888.777-66', 1001, 101, 100);
